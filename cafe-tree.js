70a61af7a689b762c9b52351a13714d4f2e93555
雷绍波 <leishaobo_cafe@163.com>
2016-09-07 17:15:37 +0800

    优化

--- cafe-tree.js
+++ cafe-tree.js
@@ -3,20 +3,69 @@
  * @param $
  */
 (function($){
-	var DEFAULT_OPTIONS={
-			isSingleCheck:false
+	var logger=function(msg){
+		console.log(msg);
+		alert(msg);
 	};
-	
-	var initIds=function(data){
+	/**
+	 * 校验参数
+	 * data 参数必须是数组，
+	 * 每个node节点,必须参数:text,href,state.checked
+	 * href 为标示id，不能传-1值，该值标示无效
+	 */
+	var validateOptionData=function(data){
+		if($.type(data)!='array' || data.length==0){
+			logger("参数必须是非空数组！");
+			return;
+		}
+		for(var i=0;i<data.length;i++){
+			var node=data[i];
+			if(node.text==null || node.text=="undefined"){
+				node.text="未命名节点";
+			}
+			if(node.href==null || node.href=="undefined"){
+				node.href=-1;
+			}else{
+				try{
+					node.href=parseInt(node.href);
+				}catch(e){
+					logger("节点 href属性必须是整形");
+				} 
+			}
+			if(node.state==null || node.state=="undefined"){
+				node.state={};
+			}
+			if(node.state.checked==null || node.state.checked=="undefined"){
+				node.state.checked=false;
+			}
+			if(node.nodes && $.type(node.nodes)=="array" && node.nodes.length > 0){
+				validateOptionData(node.nodes);
+			}
+		}
+	};
+	var initIds=function(data,parent){
 		var ids=[];
-		var getCheckedIdFromNode=function(data){
+		var getCheckedIdFromNode=function(data,parent){
+			var isCheckParent=false;
+			var checkedCount=0;
 			for(var i in data){
 				var node=data[i];
-				if(node.state && node.state.checked && node.href){
-					ids.push(parseInt(node.href));
+				if(node.state.checked){
+					checkedCount++;
+				}
+				if(node.state.checked && node.href!=-1){
+					ids.push(node.href);
 				}
 				if(node.nodes && node.nodes.length > 0){
-					getCheckedIdFromNode(node.nodes);
+					getCheckedIdFromNode(node.nodes,node);
+				}
+			}
+			if(checkedCount==data.length){
+				if(parent!=null){
+					parent.state.checked=true;
+					if(parent.href!=-1){
+						ids.push(parent.href);
+					}
 				}
 			}
 		}
@@ -34,15 +83,16 @@
 	    },
 	    _update: function() {
 	    	 var $this=this;
-			 this.options.ids=initIds(this.options.data);
+	    	 validateOptionData(this.options.data);
+	    	 this.options.ids=initIds(this.options.data,null);
 		     this.element.treeview({
 		          data: this.options.data,
 		          showIcon: true,
 		          showTags:true,
 		          showCheckbox: true,
 		          onNodeChecked: function(event, node) {
-		        	  if(parseInt(node.href)){
-		        		  $this.options.ids.push(parseInt(node.href));
+		        	  if(node.href!=-1){
+		        		  $this.options.ids.push(node.href);
 			   		   }
 		        	  if($this.options.isSingleCheck){
 		        		  $this.singeleCheck(node);
@@ -52,8 +102,8 @@
 		        	 
 		          },
 		          onNodeUnchecked: function (event, node) {
-		        	  if(parseInt(node.href)){
-		        		  var id=parseInt(node.href);
+		        	  if(node.href!=-1){
+		        		  var id=node.href;
 		        		  var index=$.inArray(id,$this.options.ids);
 		        		  if(index!=-1){
 		        			 $this.options.ids.splice(index,1);
@@ -74,6 +124,26 @@
        	            this.element.treeview("checkNode",[childNode.nodeId,{ silent: false }]);
        		 	}
        	 	}
+	    	var siblingNodes= this.element.treeview("getSiblings",node);
+			var checkedCount=0;
+	    	for(var i=0;i<siblingNodes.length;i++){
+	    		var siblingNode=siblingNodes[i];
+	    		if(siblingNode.state.checked==true){
+	    			checkedCount++;
+	    		}
+	    	}
+	    	if(checkedCount==siblingNodes.length){
+	    		var parentNodes= this.element.treeview("getParent",node.nodeId);
+	    		if(parentNodes!=null && parentNodes!="undefined" && parentNodes.state!=null && parentNodes.state!='undefined' && !parentNodes.state.checked){
+	    			this.element.treeview("checkNode",[parentNodes.nodeId,{silent: false}]);
+	    			if(parentNodes.href!=-1){
+	    				  var index=$.inArray(parentNodes.href,this.options.ids);
+		        		  if(index!=-1){
+		        			  this.options.ids.push(parentNodes.href);
+		        		  }
+	    			}
+	    		}	
+	    	}
 		},
 		uncheck:function(node){
 	    	if(node.nodes && node.nodes.length>0){
@@ -86,8 +156,8 @@
 		singeleCheck:function(node){
 			 this.element.treeview('uncheckAll', { silent: true });
 			 this.element.treeview("checkNode",[node.nodeId,{ silent: true }]);
-			 if(parseInt(node.href)){
-				 this.options.ids=[parseInt(node.href)];
+			 if(node.href!=-1){
+				 this.options.ids=[node.href];
 			 }else{
 				 this.options.ids=[];
 			 }
